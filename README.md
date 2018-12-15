b0a14c01b7d4997807a2070eed9a27369d170a94
Pavel Dotsenko <pavel_dotsenko@hotmail.com>
2018-12-15 19:50:24 +0600

    Clear repository and create new

--- README.md
+++
@@ -1,2 +0,0 @@
-# smart-price
-Application for creating and editing price tags for stores
--- stylesheet.py
+++
@@ -1,8 +0,0 @@
-stylesheet =  {'MenuBar':     'background: #3a4055;\
-							   border-right: 2px solid #2b2f3b;',
-
-			   'MainContent': 'background: #fff',
-
-			   'MenuBtn': 'background: #3a4055;\
-			   			   color: #ccc;\
-			   			   border: 0'}
